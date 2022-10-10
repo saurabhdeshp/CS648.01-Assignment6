30d8021f637c08cfbcf047b02c328b79ade6cc9e
Saurabh Deshpande <deshpandesaurabh98@gmail.com>
2022-10-09 22:38:59 -0700

    final commit

---
+++ js/03-coinflip-redux.js
@@ -0,0 +1,9 @@
+var coinFlip;
+for (let i = 1; i <= 10; i += 1) {
+    coinFlip = Math.round(Math.random());
+    if (coinFlip) {
+        window.console.log("Heads");
+    } else {
+        window.console.log("Tails");
+    }
+}
---
+++ js/04-coinflip-streak.js
@@ -0,0 +1,9 @@
+var coinFlip;
+do {
+    coinFlip = Math.round(Math.random());
+    if (coinFlip) {
+        window.document.write("Heads<br>");
+    } else {
+        window.document.write("You finally got tails...game over!");
+    }
+} while (coinFlip);
